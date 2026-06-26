0229ed4c0e3690279146cfa97dd2a68d9356d1e8
atherix24 <253341302+aetherix24@users.noreply.github.com>
2026-06-26 07:18:05 +0000

    connection fix

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins =["*"], 
+    allow_origins =[
+    "https://vibeverdict.netlify.app"
+    ], 
     allow_credentials = True ,
     allow_methods = ["*"],
     allow_headers=["*"],
