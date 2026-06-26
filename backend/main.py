93bca8716d053328ae3dfe32eea16ee3be7dbc08
atherix24 <253341302+aetherix24@users.noreply.github.com>
2026-06-26 08:06:04 +0000

    database fix

--- backend/main.py
+++ backend/main.py
@@ -28,12 +28,9 @@ app.add_middleware(
 
 def get_db_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+        os.getenv("DATABASE_URL"),
         sslmode="require"
+    
     )
 
 @app.get("/community")
